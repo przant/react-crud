2754e22676cd6c5de764b06345da98f5be1778be
Antonio Pérez <apema2079@gmail.com>
2021-11-22 12:24:57 -0600

    Add dynamic behaviour to Table component

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,124 +1,70 @@
+import { nanoid } from 'nanoid';
 import React from 'react';
+
+const HEADERS = [
+    "",
+    "FechaSolicitud",
+    "IdOrigen",
+    "IdDestino",
+    "TipoUnidad",
+    "FolioViaje",
+    "ClaveProducto",
+    "Descripcion",
+    "Cantidad",
+    "Unidad",
+    "Peso",
+    "SistemaOrigen",
+    "FechaCreacion",
+    "IdCreacion",
+    "Procesado",
+];
+
+const DATA = [
+    { id: "mercancia-" + nanoid(), FechaSolicitud: "05.11.2021", IdOrigen: 3000, IdDestino: 3000, TipoUnidad: "TRAILER", FolioViaje: "FEA-1", ClaveProducto: 29991, Descripcion: "C D CREST COMPLETE BLA+SCOPE 2X75ML", Cantidad: 23000, Unidad: "X4G", Peso: 3000, SistemaOrigen: "NADRO", FechaCreacion: "05.07.2021", IdCreacion: "creacion-" + nanoid(), Procesado: false },
+    { id: "mercancia-" + nanoid(), FechaSolicitud: "05.11.2020", IdOrigen: 2000, IdDestino: 3000, TipoUnidad: "TRAILER", FolioViaje: "FEA-1", ClaveProducto: 30415, Descripcion: "AG MI CELAR EFFACLAUR ULTRAP-G400ML", Cantidad: 28000, Unidad: "X4G", Peso: 200, SistemaOrigen: "NADRO", FechaCreacion: "05.07.2021", IdCreacion: "creacion-" + nanoid(), Procesado: false },
+    { id: "mercancia-" + nanoid(), FechaSolicitud: "05.11.2020", IdOrigen: 2000, IdDestino: 3000, TipoUnidad: "TRAILER", FolioViaje: "FEA-1", ClaveProducto: 30632, Descripcion: "WOM ISDIN HIDRA VAG C/12 APL       ", Cantidad: 2300, Unidad: "X4G", Peso: 3500, SistemaOrigen: "NADRO", FechaCreacion: "05.07.2021", IdCreacion: "creacion-" + nanoid(), Procesado: false },
+    { id: "mercancia-" + nanoid(), FechaSolicitud: "05.11.2019", IdOrigen: 1000, IdDestino: 3000, TipoUnidad: "TRAILER", FolioViaje: "FEA-1", ClaveProducto: 30657, Descripcion: "MONTELUKAST 10MG 50 TAB        LGEN", Cantidad: 600, Unidad: "X4G", Peso: 950, SistemaOrigen: "NADRO", FechaCreacion: "05.07.2021", IdCreacion: "creacion-" + nanoid(), Procesado: false },
+    { id: "mercancia-" + nanoid(), FechaSolicitud: "05.11.2019", IdOrigen: 1000, IdDestino: 3000, TipoUnidad: "", FolioViaje: "", ClaveProducto: 31354, Descripcion: "CRA NIVEA ACLARA NATP/MANO 75ML    ", Cantidad: 23000, Unidad: "X4G", Peso: 3000, SistemaOrigen: "WMS", FechaCreacion: "05.07.2021", IdCreacion: "creacion-" + nanoid(), Procesado: false },
+]
 
 export const Table = () => {
     return (
-        <div className="row mt-5">
-        <table className="table table-striped table-hover mt-3">
-          <thead className="text-center">
-            <th></th>
-            <th>FechaSolicitud</th>
-            <th>IdOrigen</th>
-            <th>IdDestino</th>
-            <th>TipoUnidad</th>
-            <th>FolioViaje</th>
-            <th>ClaveProducto</th>
-            <th>Descripcion</th>
-            <th>Cantidad</th>
-            <th>Unidad</th>
-            <th>Peso</th>
-            <th>SistemaOrigen</th>
-            <th>FechaCreacion</th>
-            <th>IdCreacion</th>
-            <th>Procesado</th>
-          </thead>
-          <tbody className="text-center fs-5">
-            <tr>
-              <td>
-                <input type="checkbox" defaultChecked={false} value="Fecha Solicitud" />
-              </td>
-              <td>R1,C2</td>
-              <td>R1,C3</td>
-              <td>R1,C4</td>
-              <td>R1,C5</td>
-              <td>R1,C6</td>
-              <td>R1,C7</td>
-              <td>R1,C8</td>
-              <td>R1,C9</td>
-              <td>R1,C10</td>
-              <td>R1,C11</td>
-              <td>R1,C12</td>
-              <td>R1,C13</td>
-              <td>R1,C14</td>
-              <td>R1,C15</td>
-            </tr>
-            <tr>
-              <td>
-                <input type="checkbox" defaultChecked={false} value="Fecha Solicitud" />
-              </td>
-              <td>R2,C2</td>
-              <td>R2,C3</td>
-              <td>R2,C4</td>
-              <td>R2,C5</td>
-              <td>R2,C6</td>
-              <td>R2,C7</td>
-              <td>R2,C8</td>
-              <td>R2,C9</td>
-              <td>R2,C10</td>
-              <td>R2,C11</td>
-              <td>R2,C12</td>
-              <td>R2,C13</td>
-              <td>R2,C14</td>
-              <td>R2,C15</td>
-            </tr>
-            <tr>
-              <td>
-                <input type="checkbox" defaultChecked={false} value="Fecha Solicitud" />
-              </td>
-              <td>R3,C2</td>
-              <td>R3,C3</td>
-              <td>R3,C4</td>
-              <td>R3,C5</td>
-              <td>R3,C6</td>
-              <td>R3,C7</td>
-              <td>R3,C8</td>
-              <td>R3,C9</td>
-              <td>R3,C10</td>
-              <td>R3,C11</td>
-              <td>R3,C12</td>
-              <td>R3,C13</td>
-              <td>R3,C14</td>
-              <td>R3,C15</td>
-            </tr>
-            <tr>
-              <td>
-                <input type="checkbox" defaultChecked={false} value="Fecha Solicitud" />
-              </td>
-              <td>R4,C2</td>
-              <td>R4,C3</td>
-              <td>R4,C4</td>
-              <td>R4,C5</td>
-              <td>R4,C6</td>
-              <td>R4,C7</td>
-              <td>R4,C8</td>
-              <td>R4,C9</td>
-              <td>R4,C10</td>
-              <td>R4,C11</td>
-              <td>R4,C12</td>
-              <td>R4,C13</td>
-              <td>R4,C14</td>
-              <td>R4,C15</td>
-            </tr>
-            <tr>
-              <td>
-                <input type="checkbox" defaultChecked={false} value="Fecha Solicitud" />
-              </td>
-              <td>R5,C2</td>
-              <td>R5,C3</td>
-              <td>R5,C4</td>
-              <td>R5,C5</td>
-              <td>R5,C6</td>
-              <td>R5,C7</td>
-              <td>R5,C8</td>
-              <td>R5,C9</td>
-              <td>R5,C10</td>
-              <td>R5,C11</td>
-              <td>R5,C12</td>
-              <td>R5,C13</td>
-              <td>R5,C14</td>
-              <td>R5,C15</td>
-            </tr>
-          </tbody>
-        </table>
-      </div>
+        <div className="row mt-5 table-responsive">
+            <table className="table table-hover text-nowrap mt-3">
+                <thead className="text-center">
+                    <tr key="">
+                        {
+                            HEADERS.map((header) => (
+                                <th className="">{header} </th>
+                            ))
+                        }
+                    </tr>
+                </thead>
+                <tbody className="text-center">
+                    {
+
+                        DATA.map((item) => (
+                            <tr key={item.id}>
+                                <td><input type="checkbox" defaultChecked={false} value="Fecha Solicitud" /></td>
+                                <td>{item.FechaSolicitud}</td>
+                                <td>{item.IdOrigen}</td>
+                                <td>{item.IdDestino}</td>
+                                <td>{item.TipoUnidad}</td>
+                                <td>{item.FolioViaje}</td>
+                                <td>{item.ClaveProducto}</td>
+                                <td>{item.Descripcion}</td>
+                                <td>{item.Cantidad}</td>
+                                <td>{item.Unidad}</td>
+                                <td>{item.Peso}</td>
+                                <td>{item.SistemaOrigen}</td>
+                                <td>{item.FechaCreacion}</td>
+                                <td>{item.IdCreacion}</td>
+                                <td>{item.Procesado ? "Si" : "No"}</td>
+                            </tr>
+                        ))
+                    }
+                </tbody>
+            </table>
+        </div>
     )
 }
