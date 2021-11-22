256a6c6915c20d25362c12aff11e460003c20d8b
Antonio Pérez <apema2079@gmail.com>
2021-11-22 15:20:24 -0600

    Update components to render table items

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
+import React, { useState } from 'react';
 import { Search } from "./components/Search";
 import { Table } from "./components/Table";
 import { Title } from "./components/Title";
 
-function App() {
+function App({ data }) {
+
+  const [items, setItems] = useState(data);
+
   return (
     <div className="container">
       <Title />
 
       <Search />
       
-      <Table />
+      <Table items={items}/>
     </div>
   );
 }
--- src/components/Table.js
+++ src/components/Table.js
@@ -1,4 +1,3 @@
-import { nanoid } from 'nanoid';
 import React from 'react';
 
 const HEADERS = [
@@ -19,15 +18,7 @@ const HEADERS = [
     "Procesado",
 ];
 
-const DATA = [
-    { id: "mercancia-" + nanoid(), FechaSolicitud: "05.11.2021", IdOrigen: 3000, IdDestino: 3000, TipoUnidad: "TRAILER", FolioViaje: "FEA-1", ClaveProducto: 29991, Descripcion: "C D CREST COMPLETE BLA+SCOPE 2X75ML", Cantidad: 23000, Unidad: "X4G", Peso: 3000, SistemaOrigen: "NADRO", FechaCreacion: "05.07.2021", IdCreacion: "creacion-" + nanoid(), Procesado: false },
-    { id: "mercancia-" + nanoid(), FechaSolicitud: "05.11.2020", IdOrigen: 2000, IdDestino: 3000, TipoUnidad: "TRAILER", FolioViaje: "FEA-1", ClaveProducto: 30415, Descripcion: "AG MI CELAR EFFACLAUR ULTRAP-G400ML", Cantidad: 28000, Unidad: "X4G", Peso: 200, SistemaOrigen: "NADRO", FechaCreacion: "05.07.2021", IdCreacion: "creacion-" + nanoid(), Procesado: false },
-    { id: "mercancia-" + nanoid(), FechaSolicitud: "05.11.2020", IdOrigen: 2000, IdDestino: 3000, TipoUnidad: "TRAILER", FolioViaje: "FEA-1", ClaveProducto: 30632, Descripcion: "WOM ISDIN HIDRA VAG C/12 APL       ", Cantidad: 2300, Unidad: "X4G", Peso: 3500, SistemaOrigen: "NADRO", FechaCreacion: "05.07.2021", IdCreacion: "creacion-" + nanoid(), Procesado: false },
-    { id: "mercancia-" + nanoid(), FechaSolicitud: "05.11.2019", IdOrigen: 1000, IdDestino: 3000, TipoUnidad: "TRAILER", FolioViaje: "FEA-1", ClaveProducto: 30657, Descripcion: "MONTELUKAST 10MG 50 TAB        LGEN", Cantidad: 600, Unidad: "X4G", Peso: 950, SistemaOrigen: "NADRO", FechaCreacion: "05.07.2021", IdCreacion: "creacion-" + nanoid(), Procesado: false },
-    { id: "mercancia-" + nanoid(), FechaSolicitud: "05.11.2019", IdOrigen: 1000, IdDestino: 3000, TipoUnidad: "", FolioViaje: "", ClaveProducto: 31354, Descripcion: "CRA NIVEA ACLARA NATP/MANO 75ML    ", Cantidad: 23000, Unidad: "X4G", Peso: 3000, SistemaOrigen: "WMS", FechaCreacion: "05.07.2021", IdCreacion: "creacion-" + nanoid(), Procesado: false },
-]
-
-export const Table = () => {
+export const Table = ({items}) => {
     return (
         <div className="row mt-5 table-responsive">
             <table className="table table-hover text-nowrap mt-3">
@@ -43,7 +34,7 @@ export const Table = () => {
                 <tbody className="text-center">
                     {
 
-                        DATA.map((item) => (
+                        items.map((item) => (
                             <tr key={item.id}>
                                 <td><input type="checkbox" defaultChecked={false} value="Fecha Solicitud" /></td>
                                 <td>{item.FechaSolicitud}</td>
